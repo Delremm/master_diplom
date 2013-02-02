91d36d46872cfe374617376030db5ac0d7bc0df7
unknown <g1m5yv@gmail.com>
2013-02-02 16:09:22 +0600

    s

--- master_diplom_app/signals.py
+++ master_diplom_app/signals.py
@@ -4,9 +4,11 @@ from robokassa.signals import result_received
 
 def payment_received(sender, **kwargs):
     order = Order.objects.get(id=kwargs['InvId'])
+    print order.staus
     order.status = '3'
     #order.paid_sum = kwargs['OutSum']
     #order.extra_param = kwargs['extra']['my_param']
     order.save()
+    print order.status
 
 result_received.connect(payment_received)
