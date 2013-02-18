c58ebe211ff33add70c958b9231afdc4019e4f54
unknown <g1m5yv@gmail.com>
2013-02-18 17:16:35 +0600

    s

--- master_diplom_app/models.py
+++ master_diplom_app/models.py
@@ -97,7 +97,7 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, related_name='profiles')
 
     name = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=12, blank=True, null=True)
 
     def __unicode__(self):
         return self.user.email
